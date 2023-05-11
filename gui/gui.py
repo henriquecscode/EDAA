ffa51423121319f1b46eb6f456b41f9eb21efe4d
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2023-05-11 10:30:26 +0100

    Update gui.py
    Co-Authored-By: henriquecscode <40891232+henriquecscode@users.noreply.github.com>

--- gui/gui.py
+++ gui/gui.py
@@ -5,7 +5,9 @@ import random
 import os
 import webbrowser 
 import folium
+from jinja2 import Template
 from folium import plugins
+from folium.utilities import normalize
 import pandas
 import sys
 
@@ -28,16 +30,15 @@ with open(os.path.expanduser(filepath), "r") as f:
         line = [value.strip() for value in line]
         edges.append(line)
 
-airports_ids = [(int(edge[0]), int(edge[1])) for edge in edges]
+airports_ids = [(int(edge[0]), int(edge[1]), edge[4]) for edge in edges]
 
-airports_file = os.path.expanduser('./data/airports_actually_used.csv');
+airports_file = os.path.expanduser('./data/airports_actually_used.csv')
 airports = pandas.read_csv(filepath_or_buffer=airports_file, sep=',')
 
 ## Start: green
 ## End: dark red
 ## Middle: cadet blue
 def plotAirport(point, type):
-    print(point)
     '''input: series that contains a numeric named latitude and a numeric named longitude
     this function creates a CircleMarker and adds it to your this_map'''
     popup  = folium.Popup(point['name'], max_width=600, max_height=600)
@@ -49,16 +50,24 @@ def plotAirport(point, type):
     else:
         folium.vector_layers.Marker(location=[point['lat'], point['lng']], tooltip=point['airport_id'], popup = popup, icon=folium.Icon(icon='plane', color='cadetblue')).add_to(m)
 
-## Edges: light gray
-def plotFlight(edge):
-    
+def get_carrier_color(carrier):
+    hex_value = hex(hash(carrier))
+    hex_value_color = hex_value.substr(0,6) #hi, it's me. I'm the problem, it's me
+    return "#" + hex_value_color
+
+## Edges: light gray booooooo
+def plotFlight(edge,color="black"):
+    # color = get_carrier_color(edge['carrier'])
     popup = folium.Popup(edge, max_width=600, max_height=600)
-    folium.vector_layers.PolyLine(locations=edge, tooltip=edge[0], popup = popup, color='lightgray', weight=1, opacity=0.8).add_to(m)
-    
-for i, (origin, dest) in enumerate(airports_ids):
+    line = folium.vector_layers.PolyLine(locations=edge, tooltip=edge[0], popup = popup, color=color, weight=2.5, opacity=0.8)
+    line.add_to(m)
+
+
+for i, (origin, dest, carrier) in enumerate(airports_ids):
     origin_airport = airports.loc[airports['airport_id'] == origin].reset_index()
     dest_airport = airports.loc[airports['airport_id'] == dest].reset_index()
     edge = [[origin_airport['lat'][0], origin_airport['lng'][0]], [dest_airport['lat'][0], dest_airport['lng'][0]]]
+    #color = get_carrier_color(carrier)
     plotFlight(edge)
 
     if (i == 0): 
@@ -70,6 +79,7 @@ for i, (origin, dest) in enumerate(airports_ids):
     else:
         plotAirport(origin_airport, AirportType.MIDDLE)
         plotAirport(dest_airport, AirportType.MIDDLE)
+
 
 # DayofMonth,DayOfWeek,Carrier,OriginAirportID,DestAirportID,DepDelay,ArrDelay,distance,flight_time
 # airports = pandas.read_csv(filepath_or_buffer='data/airports_actually_used.csv', sep=',')
